ee2d72e0b6567e5f80049d452c8400ad5f49b79e
Daniel Rodrigues <daniel.rodrigues@seegno.com>
2024-10-17 21:56:35 +0100

    Add jest config files

---
+++ jest-cov.config.cjs
@@ -0,0 +1,4 @@
+module.exports = {
+  ...require('./jest.config.cjs'),
+  testRegex: '.spec.ts$',
+};
---
+++ jest-e2e.config.cjs
@@ -0,0 +1,4 @@
+module.exports = {
+  ...require('./jest.config.cjs'),
+  testRegex: '\\.e2e-spec\\.ts$',
+};
