990b496b01fffecba7ff769565255bda1d9d8a27
Daniel Rodrigues <danielg.rodrigues42@gmail.com>
2024-12-21 17:10:54 +0000

    Improve database config

--- env/dev.env.ts
+++ env/dev.env.ts
@@ -1,1 +1,13 @@
-export default () => ({});
+export default () => ({
+  database: {
+    connection: {
+      database: 'postgres',
+      host: 'aws-0-eu-west-3.pooler.supabase.com',
+      port: 5432,
+    },
+    driver: 'postgres',
+  },
+  graphql: {
+    debug: true,
+  },
+});
--- env/prd.env.ts
+++ env/prd.env.ts
@@ -1,4 +1,12 @@
 export default () => ({
+  database: {
+    connection: {
+      database: 'postgres',
+      host: 'aws-0-eu-west-3.pooler.supabase.com',
+      port: 5432,
+    },
+    driver: 'postgres',
+  },
   graphql: {
     debug: false,
   },
